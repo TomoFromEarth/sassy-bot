bdf9c18e37b105878d96773d957ab1825b45faae
TomoFromEarth <mikedonothackmethanks@gmail.com>
2022-07-15 19:16:07 -0400

    added working function that returns random response

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-const responses =
+const sassyBot = () =>
 {
-    
+    const response = [];
+    const subject = ['I', 'They', 'We'];
+    const verb = ['cannot', 'do not', 'will not'];
+    const object = ['listen to your nonsense.', 'with you humon...', 'understand pointless words.'];
+
+    response.push(subject[Math.floor(Math.random() * subject.length)]);
+    response.push(Math.floor(Math.random() * verb.length));
+    response.push(Math.floor(Math.random() * object.length));
+
+    return response.join(' ');
 }
+
+console.log(sassyBot());
