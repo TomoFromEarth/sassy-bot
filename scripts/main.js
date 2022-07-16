d5bc0bf7c565b74164a0ccbbd37edbf667349b0b
TomoFromEarth <mikedonothackmethanks@gmail.com>
2022-07-16 03:55:31 -0400

    This is my final submission for approval until new assets are done at which point we will make the design responsive.

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,21 @@
+
 // function to return our random message
-const onButtonClick = () => {
-    const generateRandomSentence = ()=>
+const onButtonClick = () =>
+{
+    const generateRandomSentence = () =>
     {
         const response = [];
-        const subject = ['I', 'They', 'We'];
-        const verb = ['cannot', 'do not', 'will not'];
-        const object = ['listen to your nonsense.', 'with you humon...', 'understand pointless words.'];
+        
+        const subject = ['I', 'We', 'You', 'As a certified JEEN-yus, I', 'Clearly, I', 'Do you really think that I', 'It is shocking that you thought I'];
+        
+        const verb = ['cannot', 'do not', 'will not', 'probably will not'];
+        
+        const object = ['listen to your nonsense.', 'with you humon...', 'with me humon...', 'with us humon...', 'understand pointless words.', 'listen to my nonsense.', 'listen to our nonsense.'];
 
         response.push(subject[Math.floor(Math.random() * subject.length)]);
+        
         response.push(verb[Math.floor(Math.random() * verb.length)]);
+        
         response.push(object[Math.floor(Math.random() * object.length)]);
         
         const result = response.join(' ');
